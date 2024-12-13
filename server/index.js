600bed190774d9bee843db7c98ccb384110d219c
ussyalfaks <usmanalfaki@gmail.com>
2024-12-13 11:11:40 +0100

    Removed

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ dotenv.config();
 
 
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+//  const __dirname = dirname(__filename);
 
 
 const app = express();
