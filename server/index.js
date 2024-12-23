ec87a23c4135a3900f784bb71af591403e6d4269
ussyalfaks <usmanalfaki@gmail.com>
2024-12-23 14:48:37 +0100

    fixing recipe

--- server/index.js
+++ server/index.js
@@ -26,7 +26,7 @@ app.get('/', (req, res) => {
 
 app.use(cors({
   origin: [
-    'https://recipe-sharing-rouge.vercel.app/'
+    'https://recipe-sharing-rouge.vercel.app'
 
   ],
   credentials: true,
