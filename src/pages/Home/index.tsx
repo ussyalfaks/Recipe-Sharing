dcbe2e51eae6f9c9f43636188f86dcd5ed34ea4d
ussyalfaks <usmanalfaki@gmail.com>
2024-12-25 16:57:58 +0100

    fixing layout

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -23,7 +23,7 @@ const Home = () => {
 
   return (
     <div className="px-40 flex flex-1 justify-center py-5">
-      <div className="layout-content-container flex flex-col max-w-[960px] flex-1">
+      <div className=" flex flex-col max-w-7xl flex-1">
         <div className="@container">
           <div className="@[480px]:p-4">
             <div
