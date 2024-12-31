6fe5b1b990273b0c520389c01be690f55651527c
ussyalfaks <usmanalfaki@gmail.com>
2024-12-31 17:58:53 +0100

    fix: adjust backend file size limit to resolve "File size too large" issue

--- server/index.js
+++ server/index.js
@@ -38,8 +38,10 @@ app.use(cors({
 }));
  
 app.options('*', cors());
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
 app.use(cookieParser());
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
+
 
 app.use('/api/auth', authRoutes);
 app.use('/api/recipes', recipeRoutes);
